2b626788c8c8456c304dc05de632666b9d82136a
Aniket Paul <aniket.paul618@gmail.com>
2026-04-29 12:18:39 +0530

    feat: implement notification service for fetching and managing read status

--- client/src/services/notificationServices.js
+++ client/src/services/notificationServices.js
@@ -1,24 +1,24 @@
-import axios from "axios";
+import axiosBase from "./axiosBase";
 
-const API_URL = "/api/notifications";
+const API_URL = "/notifications";
 
 const getNotifications = async () => {
-  const response = await axios.get(API_URL);
+  const response = await axiosBase.get(API_URL);
   return response.data;
 };
 
 const markRead = async (id) => {
-  const response = await axios.put(`${API_URL}/${id}/read`);
+  const response = await axiosBase.put(`${API_URL}/${id}/read`);
   return response.data;
 };
 
 const markAllRead = async () => {
-  const response = await axios.put(`${API_URL}/read-all`);
+  const response = await axiosBase.put(`${API_URL}/read-all`);
   return response.data;
 };
 
 const clearAll = async () => {
-  const response = await axios.delete(API_URL);
+  const response = await axiosBase.delete(API_URL);
   return response.data;
 };
 
